8db15c2a015b15b53cdf33173fa86dc9cd027c64
Juan David López Mejía <Jlopezm@intergrupo.com>
2020-08-10 17:05:08 -0500

    Add logic to add a new product to the state

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -6,7 +6,29 @@ import {
 
 // Create new products
 export function createNewProductAction( product ) {
-  return () => {
-    console.log(product);
+  return (dispatch) => {
+    dispatch( addProduct() );
+
+    try {
+      dispatch( addProductSuccess(product) );
+    } catch (error) {
+      dispatch( addProductError(true) );
+    }
   }
 }
+
+const addProduct = () => ({
+  type: ADD_PRODUCT,
+  payload: true
+})
+
+// if the product is saved in the database
+const addProductSuccess = product => ({
+  type: ADD_PRODUCT_SUCCESS,
+  payload: product
+})
+
+// There was a error
+const addProductError = () => ({
+
+});
