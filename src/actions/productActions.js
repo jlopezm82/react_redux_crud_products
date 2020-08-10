c536574fd48a7899ae8d8066dc98070714887189
Juan David López Mejía <Jlopezm@intergrupo.com>
2020-08-10 15:54:55 -0500

    Action definition and use useDispatch

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -3,3 +3,10 @@ import {
   ADD_PRODUCT_SUCCESS,
   ADD_PRODUCT_ERROR
 } from '../types';
+
+// Create new products
+export function createNewProductAction() {
+  return () => {
+    console.log('From action')
+  }
+}
--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,6 +1,30 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux'; // useDispatch -> To execution the actions
+                                                        // useSelector -> To access the state into the component
+
+//Redux Actions
+import { createNewProductAction } from '../actions/productActions';
 
 const NewProduct = () => {
+
+  // use useDispatch and it create a function
+  const dispatch = useDispatch();
+
+  // To communicate with the actions
+  // Call the action from productAction
+  const addProduct = () => dispatch( createNewProductAction() );
+
+  const submitNewProduct = (event) => {
+    event.preventDefault();
+
+    // Check form
+
+    // Check errors
+
+    // Create new product
+    addProduct();
+  }
+
   return (
     <div className="row justify-content-center">
       <div className="col-md-8">
@@ -9,7 +33,8 @@ const NewProduct = () => {
             <h2 className="text-center mb-4 font-weight-bold">
               Add New Product
             </h2>
-            <form>
+            <form
+              onSubmit={submitNewProduct}>
               <div className="form-group">
                 <label htmlFor="">Name</label>
                 <input
