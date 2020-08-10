dd34095256979959827c6ea2831151199830b536
Juan David López Mejía <Jlopezm@intergrupo.com>
2020-08-10 11:52:11 -0500

    Install react-router-dom and Add Products.js, NewProduct.js and EditProduct.js components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
+import Products from './components/Products';
+import NewProduct from './components/NewProduct';
+import EditProduct from './components/EditProduct';
+
 
 function App() {
   return (
-    <Fragment>
+    <Router>
       <Header />
-    </Fragment>
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={Products} />
+          <Route exact path="/products/new" component={NewProduct} />
+          <Route exact path="/products/edit/:id" component={EditProduct} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
